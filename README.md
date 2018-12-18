d7c91e3233960c44971197f9d17bc3561d925f82
Egor A. Fisher <efisher@griddynamics.com>
2018-12-18 14:51:54 +0300

    relocate from origin at Tue Dec 18 14:51:54 MSK 2018

--- README.md
+++ README.md
@@ -1,3 +1,90 @@
-# zabbix-review
-https://habr.com/company/pt/blog/433126/
-https://gitlab.com/devopshq/zabbix-review-export
+# Zabbix Review and Export
+With Zabbix review and export (backup) you can create review mechanism and save zabbix configuration as code (Monitoring as Code)
+
+You can only export (backup) all hosts templates and other object with `zabbix-export.py` script.
+
+- [Requirements](#requirements)
+- [Make export and backup](#make-export-and-backup)
+- [Make review](#make-review)
+  - [Notes](#notes)
+- [Supported objects](#supported-objects)
+- [Known issues](#known-issues)
+- [Screenshots](#screenshots)
+
+# Requirements
+- Installed [Python >=3.4](https://www.python.org/downloads/)
+
+
+If you want use [review (or Monitoring as Code](#make-review):
+- [GitLab](https://gitlab.com/) - you own instance with configured [GitLab CI](https://docs.gitlab.com/ee/ci/) or cloud account
+- `git`
+
+
+## Make export and backup
+It's simple to start use this script as backup mechanism:
+```bash
+# git clone THIS_REPO or download and unpack archive
+
+python -mpip install -r requirements.xml
+
+# smoke test :)
+python ./zabbix-export.py --help
+
+# backup to current folder, save XML and JSON
+python ./zabbix-export.py --zabbix-url https://zabbix.example.com --zabbix-username user --zabbix-password password
+
+# backup zabbix configuration in YAML format
+python ./zabbix-export.py --save-yaml --zabbix-url https://zabbix.example.com --zabbix-username user --zabbix-password password
+
+# backup to custom folder in YAML format
+python ./zabbix-export.py --save-yaml --directory /home/username/path/to/zabbix-yaml --zabbix-url https://zabbix.example.com --zabbix-username user --zabbix-password password
+```
+
+## Make review
+You want to make review (Moniroting as Code). Read more on habr.com: [RU](#), [EN translated](#)
+1. Fork this repository to you GitLab account or instance (e.g. `groupname/zabbix-review-export`)
+2. Create repository where will be saved XML and YAML (e.g. two repositories `groupname/zabbix-xml` and `groupname/zabbix-yaml`. Do first (init) commit (create empty `README.md`).
+3. Create two branches in this repos: `master` and `develop`. In repository `groupname/zabbix-xml` set `develop` as a [default branch](https://docs.gitlab.com/ee/user/project/repository/branches/#default-branch).
+4. Specify [Project Variables](https://docs.gitlab.com/ee/ci/variables/#variables) for all variables, specified on top of [.gitlab-ci.yml](./.gitlab-ci.yml)
+5. Change jobs in `.gitlab-ci.yml` and leave the ones you need job in `.gitlab-ci.yml` and change to you environment (see commented examples block).
+6. Try to run manual job `YAML zabbix`
+7. Create merge request `develop=>master` in `zabbix-yaml`. For first time you can merge without review, it's too hard :)
+8. Configure [Schedule](https://docs.gitlab.com/ee/user/project/pipelines/schedules.html) (eg. every week)
+9. Change some host, template or other [supported objects](#supported-objects) in zabbix, run manual job and create merge request again. Enjoy!
+
+### Notes
+Use two repositories for XML+JSON (raw-format) and readable YAML format:
+- `XML` + `JSON` will be useful if you want restore some object after remove or alarge number of changes.
+- `YAML` format is more suitable for people to read and review changes. The script removes all empty values.
+
+Create empty merge request `develop=>master` after merge and receive notifications at changes (schedule or manual jobs run) on your email.
+
+To answer for the question "Who make this changes?" you need use [Zabbix Audit](https://www.zabbix.com/documentation/4.0/manual/web_interface/frontend_sections/reports/audit). It's difficult but possible.
+
+## Supported objects
+Use standart [zabbix export functional](https://www.zabbix.com/documentation/4.0/manual/api/reference/configuration/export):
+- hosts
+- templates
+- screen
+
+Representing objects as JSON using the API:
+- mediatypes
+- actions
+
+## Known issues
+- [ZBX-15175](https://support.zabbix.com/browse/ZBX-15175): Zabbix export - host's xml does not contain overrides or diff to templates (e.g. item's storage period, trigger.priority, trigger.status=disables\enabled)
+- [ZBXNEXT-4862](https://support.zabbix.com/browse/ZBXNEXT-4862): The implementation of functionality in Zabbix. Zabbix configuration as code - save XML in git repository
+
+
+## Screenshots
+YAML change action:
+![yaml-change-action.png](./docs/yaml-change-action.png)
+
+YAML change trigger expression:
+![yaml-change-trigger-expression.png](./docs/yaml-change-trigger-expression.png)
+
+YAML link template
+![yaml-link-template.jpg](./docs/yaml-link-template.jpg)
+
+XML change templates (but we recommend use YAML for review and XML only for backup):
+![xml-change-templates.jpg](./docs/xml-change-templates.jpg)
---
+++ zabbix-export.py
@@ -0,0 +1,215 @@
+import argparse
+import json
+import logging
+import os
+import re
+import sys
+import xml.dom.minidom
+from collections import OrderedDict
+
+import anymarkup
+import urllib3
+import yaml
+from pyzabbix import ZabbixAPI
+
+urllib3.disable_warnings()
+
+
+def remove_none(obj):
+    """
+    Remove None value from any object
+    As is from https://stackoverflow.com/a/20558778/6753144
+    :param obj:
+    :return:
+    """
+    if isinstance(obj, (list, tuple, set)):
+        return type(obj)(remove_none(x) for x in obj if x is not None)
+    elif isinstance(obj, dict):
+        return type(obj)((remove_none(k), remove_none(v))
+                         for k, v in obj.items() if k is not None and v is not None)
+    else:
+        return obj
+
+
+def get_zabbix_connection(zbx_url, zbx_user, zbx_password):
+    """
+    Sometimes pyzabbix and py-zabbix library can replace each other.
+    This is a wrapper, we don't care about what pip-module we install.
+    Return ZabbixAPI object
+    """
+    # pyzabbix library, with user\password in login method. It's GOOD library
+    logging.debug("Try connect to Zabbix by pyzabbix...")
+    try:
+        zbx_pyzabbix = ZabbixAPI(zbx_url)
+        zbx_pyzabbix.session.verify = False
+        zbx_pyzabbix.login(zbx_user, zbx_password)
+        return zbx_pyzabbix
+    except Exception as e:
+        logging.exception(e)
+
+    # py-zabbix library, with user\password in ZabbixAPI
+    logging.debug("Try connect to Zabbix by py-zabbix...")
+    try:
+        zbx_py_zabbix = ZabbixAPI(zbx_url, user=zbx_user, password=zbx_password)
+        zbx_py_zabbix.session.verify = False
+        return zbx_py_zabbix
+    except Exception as e:
+        logging.exception(e)
+    # choose good API
+
+    raise Exception("Some error in pyzabbix or py_zabbix module, see logs")
+
+
+def order_data(data):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            data[key] = order_data(value)
+        return OrderedDict(sorted(data.items()))
+    elif isinstance(data, list):
+        return [order_data(x) for x in data]
+    else:
+        return data
+
+
+def dumps_json(object, data, directory, key='name', save_yaml=False):
+    """
+    Create JSON or yaml file in folder
+    """
+    subfolder = os.path.join(directory, object)
+    if not os.path.exists(subfolder):
+        os.makedirs(subfolder)
+
+    data = order_data(data)
+
+    for item in data:
+        txt = json.dumps(item, indent=4)
+
+        # Remove bad characters from name
+        name = item[key]
+        name = re.sub(r'[\\/:"*?<>|]+', ' ', name)
+        filename = '{}/{}.{}'.format(subfolder, name, 'yaml' if save_yaml else 'json')
+        filename = os.path.abspath(filename)
+
+        logging.debug("Write to file '{}'".format(filename))
+
+        if save_yaml:
+            txt = convert_to_yaml_without_none(txt)
+
+        with open(filename, mode="w", encoding='utf-8', newline='\n') as file:
+            file.write(txt)
+
+
+def convert_to_yaml_without_none(txt):
+    """
+    Convert any object to OrderDict without None value
+    """
+
+    raw = anymarkup.parse(txt)
+    raw = remove_none(raw)
+    represent_dict_order = lambda self, data: self.represent_mapping('tag:yaml.org,2002:map', data.items())  # noqa
+    yaml.add_representer(OrderedDict, represent_dict_order)
+    txt = yaml.dump(raw, default_flow_style=False, width=10000, allow_unicode=True)
+    return txt
+
+
+def dump_xml(object, txt, name, directory, save_yaml=False):
+    """
+    Create XML or YAML in folder
+    """
+    folder = os.path.join(directory, object)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    # Remove bad characters from name
+    name = re.sub(r'[\\/:"*?<>|]+', ' ', name)
+    filename = '{}/{}.{}'.format(folder, name, 'yaml' if save_yaml else 'xml')
+    filename = os.path.abspath(filename)
+
+    # Remove bad lines from content
+    # date
+    txt = re.sub(r'<date>.*<\/date>', '', txt)
+    # zabbix.version
+    # txt = re.sub(r'<version>.*<\/version>', '', txt)
+
+    # ppretty xml
+    xml_ = xml.dom.minidom.parseString(txt)  # or xml.dom.minidom.parseString(xml_string)
+    txt = xml_.toprettyxml(indent='  ', encoding='UTF-8')
+    txt = txt.decode()
+
+    # replace xml quot to normal readable "
+    txt = txt.replace('&quot;', '"')
+
+    if save_yaml:
+        txt = convert_to_yaml_without_none(txt)
+
+    logging.debug("Write to file '{}'".format(filename))
+    with open(filename, mode="w", encoding='utf-8', newline='\n') as file:
+        file.write(txt)
+
+
+def main(zabbix_, save_yaml, directory):
+    # XML
+    # Standart zabbix xml export via API
+    def export(zabbix_api, type, itemid, name):
+        """
+        Export one type: hosts, template, screen or other
+        https://www.zabbix.com/documentation/4.0/manual/api/reference/configuration/export
+        """
+        logging.info("Export {}".format(type))
+        items = zabbix_api.get()
+        for item in items:
+            logging.info("Processing {}...".format(item[name]))
+            txt = zabbix_.configuration.export(format='xml', options={type: [item[itemid]]})
+            dump_xml(object=type, txt=txt, name=item[name], save_yaml=save_yaml, directory=directory)
+
+    if yaml:
+        logging.info("Convert all format to yaml")
+
+    logging.info("Start export XML part...")
+    export(zabbix_.host, 'hosts', 'hostid', 'name')
+    export(zabbix_.template, 'templates', 'templateid', 'name')
+    export(zabbix_.valuemap, 'valueMaps', 'valuemapid', 'name')
+    export(zabbix_.screen, 'screens', 'screenid', 'name')
+
+    # JSON
+    # not support `export` method
+    # Read more in https://www.zabbix.com/documentation/4.0/manual/api/reference/configuration/export
+    logging.info("Start export JSON part...")
+    logging.info("Processing action...")
+    actions = zabbix_.action.get(selectOperations='extend', selectFilter='extend')
+    dumps_json(object='actions', data=actions, save_yaml=save_yaml, directory=directory)
+
+    logging.info("Processing mediatypes...")
+    mediatypes = zabbix_.mediatype.get(selectUsers='extend')
+    dumps_json(object='mediatypes', data=mediatypes, key='description', save_yaml=save_yaml, directory=directory)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--zabbix-url", action="store", required=True)
+    parser.add_argument("--zabbix-username", action="store", required=True)
+    parser.add_argument("--zabbix-password", action="store", required=True)
+
+    parser.add_argument("--directory", action="store", default='./',
+                        help="Directory where exported files will be saved")
+
+    parser.add_argument("--save-yaml", action="store_true", help="All file's formats will be converted to YAML format")
+
+    args = parser.parse_args()
+    return args
+
+
+def init_logging(level=logging.INFO):
+    logger_format_string = '%(levelname)-8s %(message)s'
+    logging.basicConfig(level=level, format=logger_format_string, stream=sys.stdout)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    init_logging()
+
+    zabbix_ = get_zabbix_connection(args.zabbix_url, args.zabbix_username, args.zabbix_password)
+
+    logging.info("All files will be save in {}".format(os.path.abspath(args.directory)))
+    main(zabbix_=zabbix_, save_yaml=args.save_yaml, directory=args.directory)
